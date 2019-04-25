8915a64324b77e2ac3d195e08991dbc50edbb4c3
piglet166 <35857842+piglet166@users.noreply.github.com>
2019-04-25 13:24:14 -0400

    Switched main from Display to Driver

--- src/Display.java
+++ src/Display.java
@@ -1,18 +1,4 @@
 
 public class Display {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			World room = new World();
-			Tile[][] map = room.getTiles();
-			
-			for(int i = 0; i < map.length; i++) {
-				for(int j = 0; j < map[i].length; j++) {
-					System.out.print("["+map[i][j].getFloor()+"] ");
-				}
-				System.out.println();
-			}
-
-	}
-
 }
