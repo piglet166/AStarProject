7f2cfdf74955bd3ac6d5c47cc0fed0f8cc20604c
piglet166 <35857842+piglet166@users.noreply.github.com>
2019-04-25 13:36:16 -0400

    started JFrame

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class Display {
+	
+	private static JFrame window;
+	
+	public Display() {
+		createWindow();
+	}
+	
+	private void createWindow() {
+		window = new JFrame("A* Map");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		window.setLocationRelativeTo(null);
+		window.pack();
+		window.setVisible(true);
+	}
 
 }
--- src/Driver.java
+++ src/Driver.java
@@ -3,7 +3,20 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		World room = new World();
+		Tile[][] map = room.getTiles();
+		
+		Display window = new Display();
+		
+		
+		
+		
+		//console display for testing purposes
+		for(int i = 0; i < map.length; i++) {
+			for(int j = 0; j < map[i].length; j++) {
+				System.out.print("["+map[i][j].getFloor()+"] ");
+			}
+			System.out.println();
+		}
 	}
-
 }
