4a0b4be0ff3d74d39a7b94a5d242d789b39619f9
piglet166 <35857842+piglet166@users.noreply.github.com>
2019-04-26 12:58:34 -0400

    Update to Display for buttons

--- src/Display.java
+++ src/Display.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 public class Display{
 	
 	private static JFrame window;
+	JRadioButton b1, b2;
+	ButtonGroup bg;
 	
 	public Display(World w) {
 		createWindow(w);
@@ -13,7 +15,21 @@ public class Display{
 		World room = w;
 		Tile[][] map = room.getTiles();
 		window = new JFrame();
+		
 		window.setLayout(new GridLayout(15, 15));
+		
+		//user selects between goal and start
+		b1 = new JRadioButton("Adventurer");
+		b1.setBounds(10, 10, 60, 30);
+		window.add(b1);
+		b2 = new JRadioButton("Goal");
+		b2.setBounds(10, 10, 60, 30);
+		window.add(b2);
+		bg = new ButtonGroup();
+		bg.add(b1);
+		bg.add(b2);
+		
+		
 		
 		//set up tiles
 		for(int i = 0; i < map.length; i++) {
@@ -32,7 +48,7 @@ public class Display{
 		window.setTitle("A* Map");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLocationRelativeTo(null);
-		window.pack();
+		window.setSize(480, 540);
 		window.setVisible(true);
 	}
 
